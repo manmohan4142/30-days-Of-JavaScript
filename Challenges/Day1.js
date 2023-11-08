c5d1e1c93a8326c44f919c049fb463bbf5265bbe
Manmohan Dhal <96486531+manmohan4142@users.noreply.github.com>
2023-11-08 20:17:41 +0530

    Day1.js

---
+++ Challenges/Day1.js
@@ -0,0 +1,12 @@
+/*
+## Challenge 1:
+Write a JavaScript function that reverses a number.
+Sample Data and output:
+Example: 
+x = 32243;
+Expected Output: 34223
+*/
+
+let num1 = 32243; 
+let result = num1.toString().split('').reverse().join(''); 
+console.log(result);
